e1a4ba3783c55689d9b065c202c9175ed0a519cb
Zudsky <89594950+zuDskyy@users.noreply.github.com>
2026-07-16 07:06:45 +0400

    feat: initial SlotGame ASP.NET Core API implementation

--- SlotGame.IntegrationTests/obj/Debug/net10.0/SlotGame.IntegrationTests.AssemblyInfo.cs
+++ SlotGame.IntegrationTests/obj/Debug/net10.0/SlotGame.IntegrationTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SlotGame.IntegrationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c5c6bd3f5dfef5507c4dca3fdc27728ac27d294")]
 [assembly: System.Reflection.AssemblyProductAttribute("SlotGame.IntegrationTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SlotGame.IntegrationTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
